85b5c8434ce0ba762489584cf9675f769259976b
Mariam Babar Khan <mariamk.bscs21seecs@seecs.edu.pk>
2024-08-01 22:18:32 +0500

    home page complete without animations

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -8,8 +8,8 @@ const Banner = () => {
         className='relative flex items-center justify-center h-screen bg-auto bg-center mt-10'
         style={{ backgroundImage: "url('https://shopblissfulbeauty.com/cdn/shop/files/banner_5.png?v=1693455474&width=1500')" }} // Replace with your image path
       >
-        <div className='absolute inset-50 bg-white bg-opacity-100 w-auto h-auto rounded-3xl flex flex-col items-center justify-center p-6'>
-          <h1 className='text-6xl font-body text-black mb-10 tracking-wide'>Blissful</h1>
+        <div className='absolute inset-50 bg-white bg-opacity-100 w-auto h-auto rounded-3xl flex flex-col items-center justify-center p-8'>
+          <h1 className='text-4xl font-bodyBold text-black mb-10 tracking-wide'>Blissful</h1>
           <h2 className='text-md text-black font-body mb-6 tracking-widest'>
             More Than Skincare, It's a Lifestyle of Radiance
           </h2>
