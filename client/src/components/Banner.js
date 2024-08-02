9a38e4c1575d4a6937bbb85e48fae9df12b38714
Mariam Babar Khan <mariamk.bscs21seecs@seecs.edu.pk>
2024-08-02 19:15:32 +0500

    Home Page Complete with Animations

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -5,13 +5,13 @@ import ShopNowBtn from './ShopNowBtn';
 
 const fadeInUp = {
   hidden: { opacity: 0, y: 50 },
-  visible: { opacity: 1, y: 0 }
+  visible: { opacity: 1, y: 150 }
 };
 
 const Banner = () => {
   const { ref: bannerRef, inView: bannerInView } = useInView({
-    triggerOnce: false, 
-    threshold: 0.1 
+    triggerOnce: true,
+    threshold: 0.1
   });
 
   return (
@@ -23,11 +23,12 @@ const Banner = () => {
         variants={fadeInUp}
         transition={{ duration: 1, ease: "easeOut" }}
         className='relative flex items-center justify-center h-screen bg-auto bg-center mt-10'
-        style={{ backgroundImage: "url('https://shopblissfulbeauty.com/cdn/shop/files/banner_5.png?v=1693455474&width=1500')"}}
       >
-        <div className='absolute inset-50 bg-white bg-opacity-100 w-auto h-auto rounded-3xl flex flex-col items-center justify-center p-8'>
-          <h1 className='text-4xl font-bodyBold text-black mb-10 tracking-wide'>Blissful</h1>
-          <h2 className='text-md text-black font-body mb-6 tracking-widest'>
+        <img src="//shopblissfulbeauty.com/cdn/shop/files/banner_5.png?v=1693455474&amp;width=3840" alt="" srcset="//shopblissfulbeauty.com/cdn/shop/files/banner_5.png?v=1693455474&amp;width=375 375w, //shopblissfulbeauty.com/cdn/shop/files/banner_5.png?v=1693455474&amp;width=550 550w, //shopblissfulbeauty.com/cdn/shop/files/banner_5.png?v=1693455474&amp;width=750 750w, //shopblissfulbeauty.com/cdn/shop/files/banner_5.png?v=1693455474&amp;width=1100 1100w, //shopblissfulbeauty.com/cdn/shop/files/banner_5.png?v=1693455474&amp;width=1500 1500w, //shopblissfulbeauty.com/cdn/shop/files/banner_5.png?v=1693455474&amp;width=1780 1780w, //shopblissfulbeauty.com/cdn/shop/files/banner_5.png?v=1693455474&amp;width=2000 2000w, //shopblissfulbeauty.com/cdn/shop/files/banner_5.png?v=1693455474&amp;width=3000 3000w, //shopblissfulbeauty.com/cdn/shop/files/banner_5.png?v=1693455474&amp;width=3840 3840w" height="2755" sizes="100vw" fetchpriority="high">
+        </img>
+        <div className='absolute inset-50 bg-white bg-opacity-100 w-auto h-64 rounded-3xl flex flex-col items-center justify-center p-5'>
+          <h1 className='text-6xl font-body mt-4 text-black mb-5 tracking-wide'>Blissful</h1>
+          <h2 className='text-sm text-black font-body tracking-widest'>
             More Than Skincare, It's a Lifestyle of Radiance
           </h2>
           <ShopNowBtn />
@@ -39,7 +40,7 @@ const Banner = () => {
         animate={bannerInView ? "visible" : "hidden"}
         variants={fadeInUp}
         transition={{ duration: 1, ease: "easeOut", delay: 0.3 }} // Add delay for staggered effect
-        className='tracking-wider relative flex-col h-auto mt-10 items-center justify-center p-6 text-center'
+        className='tracking-wider relative flex-col h-auto mt-44 items-center justify-center p-6 text-center mb-36'
       >
         <h1 className='text-3xl font-bodyBold text-black mb-10'> Welcome to Blissful Skincare </h1>
         <h2 className='text-md text-black font-body mb-10'>Your one stop for your skin care solutions.</h2>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () => {
     }, []);
 
     return (
-        <nav className="bg-white border-gray-200 font-body">
+        <nav className="bg-white border-gray-200 font-body z-50">
             <div className="max-w-6xl mx-auto px-4">
                 <div className="flex items-center justify-between h-20 mt-5">
                     {/* Search Icon */}
