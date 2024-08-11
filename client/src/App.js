dd62575284400dfa9b974bad32ffe8b3814dae18
Mariam Babar Khan <mariamk.bscs21seecs@seecs.edu.pk>
2024-08-11 21:00:13 +0500

    Add ScrollToTop component for smooth scrolling to top of page

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import CategoryPage from './pages/CategoryPage';
 import Checkout from './pages/Checkout';
 import SearchResults from './pages/SearchResults';
 import HomeFooter from './components/HomeFooter';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <div className="App">
           <Sale />
           <Navbar />
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/contact" element={<Contact />} />
