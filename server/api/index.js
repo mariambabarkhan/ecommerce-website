0e846b0ce8811b9e3d8a1a04cb253879ca729661
Mariam Babar Khan <mariamk.bscs21seecs@seecs.edu.pk>
2024-08-13 19:13:46 +0500

    refactor: Update CORS configuration to allow specific origins and methods

--- server/api/index.js
+++ server/api/index.js
@@ -8,9 +8,23 @@ const path = require('path');
 const emailValidator = require('email-validator');
 const dns = require('dns');
 const Product = require('../models/Product');
-
 const app = express();
-app.use(cors());
+const allowedOrigins = [
+    'https://ecommerce-website-mfxl94qj7-mariams-projects-ef704501.vercel.app'
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+}));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI)
