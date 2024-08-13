f76284566bb36543cf9948d2b688bae102814434
Mariam Babar Khan <mariamk.bscs21seecs@seecs.edu.pk>
2024-08-13 18:54:09 +0500

    server testing

--- server/api/index.js
+++ server/api/index.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGODB_URI)
     });
 
 app.get('/', (req, res) => {
-    res.send('Server is running, shukkkkarrr!');
+    res.send('Server is running!');
 });
 
 const validateDomain = (email) => {
